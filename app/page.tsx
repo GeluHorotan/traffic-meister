5941558562792f7f54e919e70461d563cf014ce3
GeluHorotan <horotangelu17@gmail.com>
2024-01-23 20:59:14 +0200

    feat: create initial select component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,42 @@
+"use client";
 import Image from "next/image";
-import { Button } from "@nextui-org/button";
 
+import { useState, useEffect } from "react";
+
+// NextUI
+import { Button } from "@nextui-org/button";
+import { Select, SelectItem } from "@nextui-org/select";
+
+//Data
 var trafficMeister = require("@/service/index");
 
 export default function Home() {
-  trafficMeister.fetchData(function (err: string, data: any) {
-    console.log(data);
-  });
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    trafficMeister.fetchData(function (err: string, data: any) {
+      setData(data);
+    });
+  }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Test
+    <main className="flex bg-cyan-600 min-h-screen w-full items-center justify-center">
+      <div className="w-1/2 flex gap-8">
+        <Select label="Select brand" className="max-w-xs">
+          {data?.map((item: any) => (
+            <SelectItem key={item.id} value={item.brand}>
+              {item.brand}
+            </SelectItem>
+          ))}
+        </Select>
+        <Select label="Select type" className="max-w-xs">
+          {data?.map((item: any) => (
+            <SelectItem key={item.id} value={item.type}>
+              {item.type}
+            </SelectItem>
+          ))}
+        </Select>
       </div>
-      <Button>Click me</Button>
     </main>
   );
 }
