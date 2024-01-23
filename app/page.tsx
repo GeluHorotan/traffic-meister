501516fc8f3e213863fa05897cc2d52f0349892b
GeluHorotan <horotangelu17@gmail.com>
2024-01-23 21:21:26 +0200

    feat: separate select items in categories

--- app/page.tsx
+++ app/page.tsx
@@ -11,26 +11,39 @@ import { Select, SelectItem } from "@nextui-org/select";
 var trafficMeister = require("@/service/index");
 
 export default function Home() {
-  const [data, setData] = useState<any>();
+  const [vehicleData, setVehicleData] = useState<any>();
+  const [dropdownItems, setDropdownItems] = useState<any>({
+    brands: [],
+    types: [],
+    colors: [],
+  });
 
   useEffect(() => {
     trafficMeister.fetchData(function (err: string, data: any) {
-      setData(data);
+      setVehicleData(data);
+
+      data?.map((item: any, i: number) => {
+        dropdownItems.brands.push(item.brand);
+        dropdownItems.types.push(item.type);
+        item.colors.map((color, i) => dropdownItems.colors.push(color));
+      });
     });
   }, []);
+
+  console.log(dropdownItems);
 
   return (
     <main className="flex bg-cyan-600 min-h-screen w-full items-center justify-center">
       <div className="w-1/2 flex gap-8">
         <Select label="Select brand" className="max-w-xs">
-          {data?.map((item: any) => (
+          {vehicleData?.map((item: any) => (
             <SelectItem key={item.id} value={item.brand}>
               {item.brand}
             </SelectItem>
           ))}
         </Select>
         <Select label="Select type" className="max-w-xs">
-          {data?.map((item: any) => (
+          {vehicleData?.map((item: any) => (
             <SelectItem key={item.id} value={item.type}>
               {item.type}
             </SelectItem>
