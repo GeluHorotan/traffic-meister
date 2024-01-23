7ebdce1955a9dd09ff7c9e2abc2312224fb7a82b
GeluHorotan <horotangelu17@gmail.com>
2024-01-23 20:34:46 +0200

    build: add nextui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Button } from "@nextui-org/button";
+
 var trafficMeister = require("@/service/index");
 
 export default function Home() {
@@ -11,6 +13,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         Test
       </div>
+      <Button>Click me</Button>
     </main>
   );
 }
